75ea4ccf62482a9c5f9764eda3cfa79fb7ef3c36
autoselff <60112867+autoselff@users.noreply.github.com>
2025-11-24 18:04:38 +0100

    java script 100% implementation

--- pages/globals/footer.js
+++ pages/globals/footer.js
@@ -1,15 +1,16 @@
 function loadFooter() {
     const footer = document.createElement('div');
     footer.className = 'footer';
-
+    
     footer.innerHTML = `
         <a href="https://store.steampowered.com/search/?developer=autoselff">STEAM</a> <a> | </a>
         <a href="https://github.com/autoselff">GITHUB</a> <a> | </a>
         <a href="https://www.youtube.com/@dazaidev">YOUTUBE</a> <a> | </a>
         <a href="https://x.com/autoselff">TWITTER</a>
-      `;
+    `;
 
-    document.body.appendChild(footer);
+    const container = document.querySelector('.container');
+    container.appendChild(footer);
 }
 
 loadFooter();
