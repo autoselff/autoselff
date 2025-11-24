82a615c10f171e7d0bae91bdea0811316db1af4d
autoselff <60112867+autoselff@users.noreply.github.com>
2025-11-24 16:58:25 +0100

    js next

--- globals/footer.js
+++ globals/footer.js
@@ -1,5 +1,15 @@
 function loadFooter() {
-    
+    const footer = document.createElement('div');
+    footer.className = 'footer';
+
+    footer.innerHTML = `
+        <a href="https://store.steampowered.com/search/?developer=autoselff">STEAM</a> <a> | </a>
+        <a href="https://github.com/autoselff">GITHUB</a> <a> | </a>
+        <a href="https://www.youtube.com/@dazaidev">YOUTUBE</a> <a> | </a>
+        <a href="https://x.com/autoselff">TWITTER</a>
+      `;
+
+    document.body.appendChild(footer);
 }
 
 loadFooter();
---
+++ globals/web-buttons.js
@@ -0,0 +1,31 @@
+function loadWebButtons() {
+    const webButtons = document.createElement('div');
+    webButtons.className = 'section';
+
+    webButtons.innerHTML = `
+        <center>
+        <a href="https://mikeklubnika.com/">
+          <img src="../res/web-buttons/mikeklubnika.gif" alt=""> 
+        </a>
+        <a href="https://www.blender.org/">
+          <img src="../res/web-buttons/blender.gif" alt=""> 
+        </a>
+        <a href="https://www.linuxmint.com/">
+          <img src="../res/web-buttons/visitmini.gif" alt=""> 
+        </a>
+        <a href="https://myanimelist.net/profile/autoself">
+          <img src="../res/web-buttons/blink.gif" alt=""> 
+        </a>
+        <a>
+          <img src="../res/web-buttons/maja.gif" alt=""> 
+        </a>
+        <a href="https://ericalfaro.dev/">
+          <img src="../res/web-buttons/eric.gif" alt="">
+        </a>
+      </center>
+      `;
+
+    document.body.appendChild(webButtons);
+}
+
+loadWebButtons();
