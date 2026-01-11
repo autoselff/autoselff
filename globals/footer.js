13a43e3ca90f9771f41c3aafcf417dc16e63458e
autoselff <60112867+autoselff@users.noreply.github.com>
2026-01-11 21:50:44 +0100

    WIRED

--- globals/footer.js
+++ globals/footer.js
@@ -3,7 +3,7 @@ function loadFooter() {
     footer.className = 'footer';
 
     footer.innerHTML = `
-        <a href="https://discord.gg/pzycKbZ32Q">DISCORD</a> <a> | </a>
+        <a href="https://discord.gg/nhW6HwreD4">DISCORD</a> <a> | </a>
         <a href="https://store.steampowered.com/search/?developer=autoselff">STEAM</a> <a> | </a>
         <a href="https://www.youtube.com/@autoselff-dev">YOUTUBE</a> <a> | </a>
         <a href="https://x.com/autoselff">TWITTER</a> <a> | </a>
