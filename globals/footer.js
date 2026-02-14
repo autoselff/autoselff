cdc5824208cc153ab7d53084cc1995f9224d0a72
autoselff <60112867+autoselff@users.noreply.github.com>
2026-02-14 10:50:41 +0100

    Update STEAM link in footer

--- globals/footer.js
+++ globals/footer.js
@@ -10,7 +10,7 @@ function loadFooter() {
 
     footer.innerHTML = `
         <a href="https://discord.gg/nhW6HwreD4">DISCORD</a> <a> | </a>
-        <a href="https://store.steampowered.com/search/?developer=autoselff">STEAM</a> <a> | </a>
+        <a href="https://store.steampowered.com/curator/45871980">STEAM</a> <a> | </a>
         <a href="https://www.youtube.com/@autoselff">YOUTUBE</a> <a> | </a>
         <a href="https://github.com/autoselff">GITHUB</a> 
     `;
