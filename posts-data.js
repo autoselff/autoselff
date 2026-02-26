6174c8adb6d7534e50dc18d494ae2beb915f1ea1
autoselff <60112867+autoselff@users.noreply.github.com>
2026-02-26 23:06:52 +0100

    Update posts-data.js

--- posts-data.js
+++ posts-data.js
@@ -1,16 +1,23 @@
 const POSTS = [
     {
+        date: "02-26-2026",
+        content: "I just uploaded a new video. In it, I briefly talk about my programming journey over the last three years. The video includes about 50% of what I consider to be my most interesting projects. Have a nice evening.",
+        link: "https://youtu.be/aQCkT5HYHXA",
+        linkText: "Watch on YouTube",
+        tags: ["announcement", "new_video"]
+    },
+    {
         date: "02-14-2026",
         content: "HyperDissonance Act 1 is now also available on itch.io! If you haven't played it yet, feel free to give it a try. The full version is still available exclusively on Steam.",
         link: "https://autoselff.itch.io/hyperdissonance",
         linkText: "Itch.io Page",
-        tags: ["announcement", "gamedev"]
+        tags: ["announcement", "devlog"]
     },
     {
         date: "01-24-2026",
         content: "Hello. I have just uploaded a new video about a procedurally animated snake,  which I of course made in C, as everything lately. Feel free to watch, and have a good day/night.",
         link: "https://youtu.be/ydlTnN03Zzo",
         linkText: "Watch on YouTube",
-        tags: ["youtube", "announcement"]
+        tags: ["announcement", "new_video"]
     }
 ];
